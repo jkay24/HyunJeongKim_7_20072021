3cdaa54abe7eff684f30e4037170122e70d48e0e
Saad Léo EL KHENIFER <saad.elkhenifer@gmail.com>
2022-02-09 20:33:58 +0100

    fix routes redirection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import "./styles/style.css";
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/login/Login";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
@@ -7,15 +7,15 @@ import Profile from "./pages/profile/Profile";
 function App() {
   return (
     <div className="app">
-      <Routes>
-        <Route path="/login" element={<Login />}></Route>
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
-  ); /* (
-    <div>
-      <Login />
-    </div>
-  ); */
+  );
 }
+
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,10 +5,8 @@ import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}></Route>
-    </Routes>
-  </BrowserRouter>,
-  document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
