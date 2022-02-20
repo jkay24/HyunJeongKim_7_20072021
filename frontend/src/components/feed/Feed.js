0a32e1a8b2ce71832fef01ef18da6939f84f2766
Annie Kim <anniehckim@gmail.com>
2022-02-19 21:59:53 -0500

    Refactor, fetch posts and user profile data

--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -5,10 +5,30 @@ import React, { useEffect, useState, useContext } from "react";
 import axios from "axios";
 import { AuthContext } from "../../context/AuthContext";
 
+const useProfileData = (user) => {
+  const [profileData, setProfileData] = useState({});
+
+  useEffect(() => {
+    const fetchUserProfile = async () => {
+      const res = await axios.get(`http://localhost:3000/api/user/${userId}`, {
+        headers: {
+          JWToken: user.token,
+        },
+      });
+      setProfileData(res.data);
+    };
+    fetchUserProfile();
+  }, []);
+
+  return profileData;
+}
+
 export default function Feed() {
-  /*  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
   const { user } = useContext(AuthContext);
-  let userId = JSON.parse(localStorage.getItem("user")).id;
+  const profileData = useProfileData(user);
+
+  // let userId = JSON.parse(localStorage.getItem("user")).id;
   useEffect(() => {
     const fetchPosts = async () => {
       const res = await axios.get(`http://localhost:3000/api/post/`, {
@@ -21,15 +41,28 @@ export default function Feed() {
       console.log(posts);
     };
     fetchPosts();
-  }, [userId]); */
+  }, []);
+
   return (
     <div className="feed">
       <div className="feedWrapper">
-        <Share />
-        <Post />
-        {/*   {posts.map((p) => (
-          <Post key={p.id} post={p} />
-        ))} */}
+        <Share profileData={profileData} />
+        <>
+          <div className="title">Publications récentes</div>
+          <hr />
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                profilePic={post.profilePic}
+                firstname={post.firstname}
+                createdAt={post.createdAt}
+                content={post.content}
+                image={post.image}
+              />
+            )
+          })}
+        </>
       </div>
     </div>
   );
